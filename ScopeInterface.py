e27d8d5086292620ec6a5415e2d702057d292cd7
aviplane <avikar96@gmail.com>
2022-06-28 09:14:46 -0700

    Post-shutdown bug fix

--- ScopeInterface.py
+++ ScopeInterface.py
@@ -9,6 +9,7 @@ import numpy as np
 import time
 from device_server import DeviceServer
 import h5py
+import threading, os
 
 process_tree = ProcessTree.instance()
 process_tree.zlock_client.set_process_name("rigol-scope")
@@ -25,8 +26,10 @@ class ScopeInterface:
                 + "Found devices:\n" + "\n".join(all_devices)
             )
         self.scope = self.resource_manager.open_resource(acceptable_devices[0])
+        #self.scope.read_termination="\n"
+        id = self.scope.query("*IDN?")
         self.n_channels = self.scope.query_ascii_values(":SYST:RAM?")[0]
-        print(f'Connected to scope with {self.n_channels} channels')
+        print(f'Connected to scope {id} with {self.n_channels} channels')
 
     def get_voltage(self, channel, stop = True):
         """
@@ -38,6 +41,7 @@ class ScopeInterface:
             the trigger pulse
             data - numpy array of voltage at each time
         """
+        print("Channel", channel)
         if channel > self.n_channels or channel < 1:
             raise Exception(
                 f"Channel must be between 1 and {self.n_channels}.")
@@ -56,8 +60,9 @@ class ScopeInterface:
         data = data[:-1]
         #time_relative_trigger = np.arange(0, -wvf_points * xinc, -xinc) - xorigin
         time_relative_trigger = np.linspace(
-            0, -(wvf_points - 1) * xinc, int(wvf_points)) - xorigin
-        return time_relative_trigger, data
+            0, (wvf_points - 1) * xinc, int(wvf_points)) - xorigin
+
+        return time_relative_trigger[:-1], data
 
     def run(self):
         self.scope.write(":RUN")
@@ -71,9 +76,10 @@ class ScopeInterface:
             time - numpy array of times that data was taken
             datas - list of voltages, corresponding to channel 1..n
         """
+        channels = np.array(channels)
         values = [self.get_voltage(i, stop) for i in channels + 1]
         times, datas = zip(*values)
-        return times[0][::-1], datas[::-1]
+        return times[0], datas
 
     def set_timestep(self, timestep):
         """
@@ -94,13 +100,14 @@ class ScopeInterface:
         """
         Set the scope horizontal offset relative to the trigger
         Input:
-            offset: float, secs [Needs to be double checked]
+            offset: float, secs
         """
         current_offset = float(self.scope.query_ascii_values(":TIM:OFFS?")[0])
+        print(f"Setting offset to {offset}")
+        print(f"Previous value was {current_offset}")
         if offset != current_offset:
             self.scope.write(f":TIM:OFFS {offset:f}")
-            time.sleep(0.1)
-        print("Set time offset")
+            time.sleep(0.05)
 
 
 class ScopeServer(DeviceServer):
@@ -130,23 +137,38 @@ class ScopeServer(DeviceServer):
         self.interface.run()
         return True
 
+    def write_voltages(self, h5_filepath):
+        times, voltages = self.interface.get_all_voltages(np.array(self.channels))
+        head, tail = os.path.split(labscript_utils.shared_drive.path_to_local(h5_filepath))
+        number = tail.split('_')[-1][:-3]
+        print(f'{head}')
+        for name, voltage in zip(self.names, voltages):
+            print(f'{head}/RIGOL{name}_{number}.npy')
+            np.save(f'{head}/RIGOL{name}_{number}.npy', voltage)
+            np.save(f'{head}/RIGOLtimes{name}_{number}.npy', times)
+
+
     def transition_to_static(self, h5_filepath):
         """
         Read scope values, write to h5 file.
         """
-        times, voltages = self.interface.get_all_voltages(self.channels)
-        # while True:
+        save_thread = threading.Thread(target=self.write_voltages, args=(h5_filepath,))
+        save_thread.start()
+        print(self.channels, self.names)
+        # times, voltages = self.interface.get_all_voltages(np.array(self.channels))
+        # print(voltages)
+        # # while True:
+        # #     try:
+        # with h5py.File(h5_filepath, 'a') as f:
+        #     group = f['data']
         #     try:
-        with h5py.File(h5_filepath, 'a') as f:
-            group = f['data']
-            try:
-                trace_group = f.create_group('ScopeTraces')
-            except ValueError:
-                trace_group = f['ScopeTraces']
-                print("Group Already existed")
-            for name, voltage in zip(self.names, voltages):
-                trace_group.create_dataset('times' + name, data=times, dtype='float')
-                trace_group.create_dataset(name, data=voltage, dtype='float')
+        #         trace_group = f.create_group('ScopeTraces')
+        #     except ValueError:
+        #         trace_group = f['ScopeTraces']
+        #         print("Group Already existed")
+        #     for name, voltage in zip(self.names, voltages):
+        #         trace_group.create_dataset('times' + name, data=times, dtype='float')
+        #         trace_group.create_dataset(name, data=voltage, dtype='float')
 
     def abort(self):
         """To be overridden by subclasses. Return cameras and any other state
